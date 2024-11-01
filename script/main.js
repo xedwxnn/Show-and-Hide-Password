4f019abd31d00e6eab7b125e75d12ebdcd58e18f
Edwxn <edwinpena0306@gmail.com>
2024-11-01 11:54:15 -0400

    Realización de diseño y opción de mostrar contraseña

---
+++ script/main.js
@@ -0,0 +1,32 @@
+let PasswordEl = document.getElementById("passwordInput");
+let EyeIconEl = document.getElementById("ImageInput");
+
+function updateEyeIcon() {
+    if (PasswordEl.value === "") {
+        EyeIconEl.src = "/assets/images/lock-icon.png";
+    }
+    
+    else {
+        EyeIconEl.src = "/assets/images/open_eye-logo.png";
+    }
+}
+
+PasswordEl.addEventListener("input", updateEyeIcon);
+        
+EyeIconEl.onclick = function() {
+    if (PasswordEl.value === "") {
+        return;
+    }
+            
+    if (PasswordEl.type === "password") {
+        PasswordEl.type = "text";
+        EyeIconEl.src = "/assets/images/close_eye-logo.png";
+    }
+    
+    else {
+        PasswordEl.type = "password";
+        EyeIconEl.src = "/assets/images/open_eye-logo.png";
+    }
+};
+
+updateEyeIcon();
